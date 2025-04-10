aa1ff5d799d4c7c039b6773ac1a62cbb4fcf8a1d
xiaoli <2585960098@qq.com>
2025-04-10 17:02:44 +0800

    feature: 优化operator-pending按键映射

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -21,10 +21,6 @@ end
 
 map({ 'v', 'n' }, 'J', '5j')
 map({ 'v', 'n' }, 'K', '5k')
-map({ 'v', 'n' }, 'H', '^')
-map({ 'v', 'n' }, 'L', 'g_')
-map('n', 'cH', 'c^')  -- 删除到行首
-map('n', 'cL', 'cg_') -- 删除到行尾
 
 
 
@@ -32,7 +28,8 @@ map('n', 'cL', 'cg_') -- 删除到行尾
 map({ 'v', 'o' }, 'w', 'iw')
 map({ 'v', 'o' }, 'ii', 'i{')
 map({ 'v', 'o' }, 'b', 'i(')
-
+map({ 'v', 'o' }, 'H', '^')
+map({ 'v', 'o' }, 'L', 'g_')
 
 map('n', '<leader>s', 'V$%')
 map('n', '<leader>a', 'za')
@@ -43,6 +40,7 @@ map("n", "<leader>nh", ":nohl<CR>")
 
 map("n", "<leader>vv", "<C-w>v") -- 水平新增窗口
 map("n", "<leader>hv", "<C-w>s") -- 垂直新增窗口
+--
 -- 窗口光标移动 左右
 map('n', '<C-[>', '<C-W>h')
 map('n', '<C-]>', '<C-W>l')
