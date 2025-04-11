8f67c8f1f605e5d597f35a022625a88fde12c26e
xiaoli <2585960098@qq.com>
2025-04-11 09:54:36 +0800

    feature: 终端插件增加

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+      -- 基础配置
+      size = 20,
+      open_mapping = [[<c-\>]], -- 默认快捷键 Ctrl+\
+      direction = 'horizontal', -- 浮动窗口
+      float_opts = {
+        border = 'float',       -- 圆角边框
+      },
+    },
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+
+      -- 添加自定义快捷键 <leader>tt 打开/关闭终端
+      vim.keymap.set('n', '<C-t>', '<CMD>ToggleTerm<CR>', {
+        noremap = true,
+        silent = true,
+        desc = 'Toggle terminal'
+      })
+    end
+  }
+}
