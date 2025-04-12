18272724be1e398cb7432e513d413b3ce7a07b3d
xiaoliyo <2585960098@qq.com>
2025-04-12 10:03:43 +0800

    feature: 修改codeium配置

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,13 +1,4 @@
 return {
-  'Exafunction/codeium.vim',
-  tag = '1.8.37',
+  'Exafunction/windsurf.vim',
   event = 'BufEnter',
-  config = function()
-    vim.g.codeium_idle_delay = 100
-
-    -- set keymaps
-    local keymap = vim.keymap
-
-    keymap.set('i', '<C-c>', '<ESC>:call codeium#Clear()<CR>', { noremap = true, silent = true })
-  end,
 }
