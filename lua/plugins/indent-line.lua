deef9828676d288193145a4c4fd348ee5725610b
xiaoliyo <2585960098@qq.com>
2025-05-02 11:18:06 +0800

    style: 修改缩进线配置

--- lua/plugins/indent-line.lua
+++ lua/plugins/indent-line.lua
@@ -4,32 +4,39 @@ return {
   lazy = true,
   event = { 'BufReadPost', 'BufNewFile' },
   config = function()
-    local highlight = {
-      "RainbowRed",
-      "RainbowYellow",
-      "RainbowBlue",
-      "RainbowOrange",
-      "RainbowGreen",
-      "RainbowViolet",
-      "RainbowCyan",
-    }
-    local hooks = require "ibl.hooks"
-    -- create the highlight groups in the highlight setup hook, so they are reset
-    -- every time the colorscheme changes
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      -- vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#0bf432" }) -- 设置为注释的绿色
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-    end)
+    -- 用mini.indentscope配置------------------------
+    local useMiniIndentScope = true
+    if useMiniIndentScope then
+      require('ibl').setup({
+        scope = {
+          enabled = false,
+        }
+      })
+      return
+    else
+      -- 不用mini.indentscope配置------------------------
+      local highlight = {
+        'RainbowGreen',
+        'RainbowYellow',
+        'RainbowBlue',
+        'RainbowOrange',
+        'RainbowViolet',
+        'RainbowCyan',
+      }
+      local hooks = require('ibl.hooks')
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#0bf432' }) -- 设置为注释的绿色
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+      end)
 
-    vim.g.rainbow_delimiters = { highlight = highlight }
-    require("ibl").setup { scope = { highlight = highlight } }
+      vim.g.rainbow_delimiters = { highlight = highlight }
+      require('ibl').setup({ scope = { highlight = highlight } })
 
-    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+    end
   end,
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,8 +3,33 @@ return {
   config = function()
     require('mini.ai').setup({
       custom_textobjects = {
+        -- ia aa 参数处理
         a = require('mini.ai').gen_spec.argument({ brackets = { '%b()', '%b[]', '%b{}' } }),
-      }
+      },
     })
-  end
+    local indent_scope_instance = require('mini.indentscope')
+    indent_scope_instance.setup({
+      draw = {
+        delay = 50,
+        predicate = function(scope)
+          return not scope.body.is_incomplete
+        end,
+        priority = 2,
+      },
+      mappings = {
+        object_scope = 'ii',
+        object_scope_with_border = 'ai',
+        goto_top = '[i',
+        goto_bottom = ']i',
+      },
+
+      options = {
+        border = 'both',
+        indent_at_cursor = true,
+        n_lines = 10000,
+        try_as_border = false,
+      },
+      symbol = '▎',
+    })
+  end,
 }
