8ecb4bd3b44b5a8cdce6011869ac3dbd13864d08
xiaoliyo <2585960098@qq.com>
2025-05-03 14:38:18 +0800

    feature: 修改ai和coc接受提示快捷键

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -70,6 +70,7 @@ end
 
 local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
 local keyset = vim.keymap.set
+keyset('i', '<TAB>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 keyset('i', '<cr>', [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 keyset('n', 'gh', '<CMD>lua _G.show_docs()<CR>', { silent = true })
 keyset('i', '<c-i>', 'coc#refresh()', { silent = true, expr = true })           -- ctrl+i触发补全 类似vscode cmd+i
