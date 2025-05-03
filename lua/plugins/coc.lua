eba7820f29bc8333019641244cc6fd28fe9df1cd
xiaoliyo <2585960098@qq.com>
2025-05-03 11:25:14 +0800

    feature: 增加coc-spell-checker配置

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -20,7 +20,8 @@ return {
       'coc-yank',
       'coc-webview',
       'coc-markdown-preview-enhanced',
-      'coc-fzf-preview'
+      'coc-fzf-preview',
+      'coc-spell-checker',
       -- 'coc-prettier',
       -- 'coc-rust-analyzer'
     }
@@ -35,13 +36,13 @@ return {
           return string.format('%s', diagnostic.message)
         end,
       },
-      signs = true,             -- 在左侧显示错误标记
-      underline = true,         -- 在错误代码下方显示下划线
+      signs = true, -- 在左侧显示错误标记
+      underline = true, -- 在错误代码下方显示下划线
       update_in_insert = false, -- 在插入模式下不更新diagnostic
       float = {
-        border = 'rounded',     -- 浮动窗口的边框样式
-        focusable = false,      -- 浮动窗口不可聚焦
-        source = 'if_many',     -- 显示错误来源
+        border = 'rounded', -- 浮动窗口的边框样式
+        focusable = false, -- 浮动窗口不可聚焦
+        source = 'if_many', -- 显示错误来源
         format = function(diagnostic)
           return string.format('%s', diagnostic.message)
         end,
@@ -52,7 +53,10 @@ return {
     local signs = { Error = '󰅙', Info = '󰋼', Hint = '󰌵', Warn = '' }
     for type, icon in pairs(signs) do
       local hl = 'DiagnosticSign' .. type
-      vim.fn.sign_define(hl, { --[[ text = icon, ]] texthl = hl, numhl = hl })
+      vim.fn.sign_define(hl, { --[[ text = icon, ]]
+        texthl = hl,
+        numhl = hl,
+      })
     end
 
     -- 使用`[g`和`]g`来导航诊断信息
@@ -74,6 +78,6 @@ return {
     vim.api.nvim_set_hl(0, 'DiagnosticFloatingHint', { fg = hint_color })
 
     vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#72c6b1', bg = '#2a2a2a' }) -- 浮动边框
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#2a2a2a' })                 -- 浮动背景色
-  end
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#2a2a2a' }) -- 浮动背景色
+  end,
 }
