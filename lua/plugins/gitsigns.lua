c1a21d0797ef777983cfa0cee8092ff42870a9b0
xiaoliyo <2585960098@qq.com>
2025-05-03 14:24:19 +0800

    feature: 优化gitsigns配置

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,12 +3,13 @@ return {
   event = 'BufRead',
   config = function()
     vim.api.nvim_set_hl(0, 'GitSignsCurrentLineBlame', {
-      fg = 'gold',  -- 使用和注释相同的颜色
-      italic = true -- 保持斜体样式（可选）
+      fg = 'gold', -- 使用和注释相同的颜色
+      italic = true, -- 保持斜体样式（可选）
     })
-    require('gitsigns').setup {
+    require('gitsigns').setup({
       current_line_blame = true,
       current_line_blame_opts = { delay = 0 },
+      current_line_blame_formatter = 'commit: <abbrev_sha>, <author>, <author_time:%R> - <summary>',
       signs = {
         add = { text = '+' },
         change = { text = '~' },
@@ -16,6 +17,25 @@ return {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
-    }
-  end
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- 当前行提交记录完整信息
+        map('n', '<leader>gb', function()
+          gitsigns.blame_line({ full = true })
+        end)
+
+        -- 当前文件提交完整记录
+        map('n', '<leader>gB', function()
+          gitsigns.blame()
+        end)
+      end,
+    })
+  end,
 }
