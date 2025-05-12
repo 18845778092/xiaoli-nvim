a608a404748ce515d65ecddb6cd5dfe6689d7449
xiaoli <2585960098@qq.com>
2025-05-12 20:22:26 +0800

    feature: 增加multi-cursor快捷键

--- lua/plugins/multi-cursor.lua
+++ lua/plugins/multi-cursor.lua
@@ -1,4 +1,11 @@
 return {
   'mg979/vim-visual-multi',
   event = 'VeryLazy',
+  init = function()
+    vim.g.VM_maps = {
+      ['Add Cursor Down'] = '<C-n>',
+      ['Add Cursor Up'] = '<C-u>',
+      ['Find Under'] = 'gb',
+    }
+  end,
 }
