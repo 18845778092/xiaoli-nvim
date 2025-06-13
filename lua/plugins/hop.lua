4c05edade4c314f78449f6082bac09e4a4465d0d
xiaoli <2585960098@qq.com>
2025-06-13 14:02:56 +0800

    chore: 简单修改杂项

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -2,21 +2,25 @@ return {
   'phaazon/hop.nvim',
   event = 'VeryLazy',
   config = function()
-    require('hop').setup({
+    local hop = require('hop')
+
+    hop.setup({
       -- 类似 flash.nvim 的配置风格
       keys = 'etovxqpdygfblzhckisuran', -- 跳转键位设置
-      jump_on_sole_occurrence = false,  -- 唯一匹配时不自动跳转
+      jump_on_sole_occurrence = false, -- 唯一匹配时不自动跳转
       -- uppercase_labels = true,          -- 使用大写字母作为标签
       -- multi_windows = true,             -- 支持多窗口跳转
     })
 
+    local hint = require('hop.hint')
+
     -- 设置快捷键
     vim.keymap.set('n', '<leader>j', function()
-      require('hop').hint_lines({ direction = require('hop.hint').HintDirection.AFTER_CURSOR })
+      hop.hint_lines({ direction = hint.HintDirection.AFTER_CURSOR })
     end, { desc = '向下跳转到行首' })
 
     vim.keymap.set('n', '<leader>k', function()
-      require('hop').hint_lines({ direction = require('hop.hint').HintDirection.BEFORE_CURSOR })
+      hop.hint_lines({ direction = hint.HintDirection.BEFORE_CURSOR })
     end, { desc = '向上跳转到行首' })
-  end
+  end,
 }
