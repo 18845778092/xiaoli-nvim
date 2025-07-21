3a46a94e7c2b9a424fa23357b90e59e83a49e238
xiaoli <2585960098@qq.com>
2025-07-21 14:50:37 +0800

    chore: delete coc related configurations

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,7 +1,5 @@
 -- auto code completion
 
-local use_coc = false
--- 结合coc使用的配置
 return {
   'hrsh7th/nvim-cmp',
   event = { 'BufReadPre', 'BufNewFile', 'VimEnter' },
@@ -34,125 +32,110 @@ return {
       local node_to_check = { 'start_tag', 'self_closing_tag', 'directive_attribute' }
       return vim.tbl_contains(node_to_check, node:type())
     end
-    if use_coc then
-      cmp.setup({
-        performance = {
-          max_view_entries = 30,
-        },
-        completion = {
-          completeopt = 'menu,menuone,preview,noselect',
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-      })
-    else
-      local luasnip = require('luasnip')
-      local lspkind = require('lspkind')
+    local luasnip = require('luasnip')
+    local lspkind = require('lspkind')
 
-      -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-      require('luasnip.loaders.from_vscode').lazy_load()
+    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    require('luasnip.loaders.from_vscode').lazy_load()
 
-      -- extend filetypes
-      luasnip.filetype_extend('typescript', { 'javascript' })
-      luasnip.filetype_extend('typescriptreact', { 'javascript' })
-      luasnip.filetype_extend('javascriptreact', { 'javascript' })
+    -- extend filetypes
+    luasnip.filetype_extend('typescript', { 'javascript' })
+    luasnip.filetype_extend('typescriptreact', { 'javascript' })
+    luasnip.filetype_extend('javascriptreact', { 'javascript' })
 
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-      end
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    end
 
-      cmp.setup({
-        performance = {
-          max_view_entries = 20,
-        },
-        completion = {
-          completeopt = 'menu,menuone,preview,noselect',
-          j,
-        },
-        snippet = { -- configure how nvim-cmp interacts with snippet engine
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
+    cmp.setup({
+      performance = {
+        max_view_entries = 20,
+      },
+      completion = {
+        completeopt = 'menu,menuone,preview,noselect',
+        j,
+      },
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
+        ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ['<C-e>'] = cmp.mapping.abort(), -- close completion window
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<TAB>'] = cmp.mapping.confirm({ select = false }),
+        ['<C-n>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }), -- i - insert mode; s - select mode
+        ['<C-p>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+      -- sources for autocompletion
+      sources = cmp.config.sources({
+        {
+          name = 'nvim_lsp',
+          ---@param entry cmp.Entry
+          ---@param ctx cmp.Context
+          entry_filter = function(entry, ctx)
+            -- Check if the buffer type is 'vue'
+            if ctx.filetype ~= 'vue' then
+              return true
+            end
+
+            -- Use a buffer-local variable to cache the result of the Treesitter check
+            local bufnr = ctx.bufnr
+            local cached_is_in_start_tag = vim.b[bufnr]._vue_ts_cached_is_in_start_tag
+            if cached_is_in_start_tag == nil then
+              vim.b[bufnr]._vue_ts_cached_is_in_start_tag = is_in_start_tag()
+            end
+            -- If not in start tag, return true
+            if vim.b[bufnr]._vue_ts_cached_is_in_start_tag == false then
+              return true
+            end
+            -- rest of the code
+
+            local cursor_before_line = ctx.cursor_before_line
+            -- For events
+            if cursor_before_line:sub(-1) == '@' then
+              return entry.completion_item.label:match('^@')
+              -- For props also exclude events with `:on-` prefix
+            elseif cursor_before_line:sub(-1) == ':' then
+              return entry.completion_item.label:match('^:') and not entry.completion_item.label:match('^:on%-')
+            else
+              return true
+            end
           end,
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-j>'] = cmp.mapping.select_next_item(), -- next suggestion
-          ['<C-k>'] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ['<C-e>'] = cmp.mapping.abort(), -- close completion window
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<TAB>'] = cmp.mapping.confirm({ select = false }),
-          ['<C-n>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }), -- i - insert mode; s - select mode
-          ['<C-p>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
+        { name = 'luasnip' }, -- snippets
+        { name = 'buffer' }, -- text within current buffer
+        { name = 'path' }, -- file system paths
+      }),
+
+      -- configure lspkind for vs-code like pictograms in completion menu
+      formatting = {
+        format = lspkind.cmp_format({
+          maxwidth = 50,
+          ellipsis_char = '...',
         }),
-        -- sources for autocompletion
-        sources = cmp.config.sources({
-          {
-            name = 'nvim_lsp',
-            ---@param entry cmp.Entry
-            ---@param ctx cmp.Context
-            entry_filter = function(entry, ctx)
-              -- Check if the buffer type is 'vue'
-              if ctx.filetype ~= 'vue' then
-                return true
-              end
-
-              -- Use a buffer-local variable to cache the result of the Treesitter check
-              local bufnr = ctx.bufnr
-              local cached_is_in_start_tag = vim.b[bufnr]._vue_ts_cached_is_in_start_tag
-              if cached_is_in_start_tag == nil then
-                vim.b[bufnr]._vue_ts_cached_is_in_start_tag = is_in_start_tag()
-              end
-              -- If not in start tag, return true
-              if vim.b[bufnr]._vue_ts_cached_is_in_start_tag == false then
-                return true
-              end
-              -- rest of the code
-
-              local cursor_before_line = ctx.cursor_before_line
-              -- For events
-              if cursor_before_line:sub(-1) == '@' then
-                return entry.completion_item.label:match('^@')
-              -- For props also exclude events with `:on-` prefix
-              elseif cursor_before_line:sub(-1) == ':' then
-                return entry.completion_item.label:match('^:') and not entry.completion_item.label:match('^:on%-')
-              else
-                return true
-              end
-            end,
-          },
-          { name = 'luasnip' }, -- snippets
-          { name = 'buffer' }, -- text within current buffer
-          { name = 'path' }, -- file system paths
-        }),
-
-        -- configure lspkind for vs-code like pictograms in completion menu
-        formatting = {
-          format = lspkind.cmp_format({
-            maxwidth = 50,
-            ellipsis_char = '...',
-          }),
-        },
-      })
-    end
+      },
+    })
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
