ffb23db665482ae9eae22da3ceee1ddb87b7b3f9
xiaoli <2585960098@qq.com>
2025-07-23 11:45:10 +0800

    feat: eslint code action

--- lua/plugins/lsp/servers/eslint.lua
+++ lua/plugins/lsp/servers/eslint.lua
@@ -1,5 +1,7 @@
 return function()
+  local capabilities = vim.lsp.protocol.make_client_capabilities()
   vim.lsp.config('eslint', {
+    capabilities = capabilities,
     settings = {
       format = false, -- 使用biome修复错误
     },
