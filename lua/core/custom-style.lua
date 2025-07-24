25ed5513c3c527c74fecaacde5309028d6c9a4b8
xiaoli <2585960098@qq.com>
2025-07-24 16:37:59 +0800

    style: vue component semantic highlight

--- lua/core/custom-style.lua
+++ lua/core/custom-style.lua
@@ -6,6 +6,7 @@ local style = {
     info_color = '#44ffff',
     hint_color = '#7fa2b8',
     active_line_num_color = '#a8fcfe',
+    vue_component_color = '#ffff44',
   },
   border = 'double',
 }
--- lua/plugins/lsp/servers/vue.lua
+++ lua/plugins/lsp/servers/vue.lua
@@ -1,4 +1,5 @@
 return function()
+  local color_table = require('core.custom-style').color_table
   local vue_language_server_path = vim.fn.expand('$MASON/packages')
     .. '/vue-language-server'
     .. '/node_modules/@vue/language-server'
@@ -47,9 +48,18 @@ return function()
       'typescriptreact',
       'vue',
     },
+    on_attach = function(client, bufnr)
+      if vim.bo[bufnr].filetype == 'vue' then
+        client.server_capabilities.semanticTokensProvider.full = false
+      end
+    end,
   }
 
-  local vue_ls_config = {}
+  local vue_ls_config = {
+    on_attach = function(client, bufnr)
+      client.server_capabilities.semanticTokensProvider.full = true
+    end,
+  }
 
   vim.lsp.config('vtsls', vtsls_config) -- vue 文件 ts部分支持
   vim.lsp.config('vue_ls', vue_ls_config) -- vue文件 html css支持
@@ -58,5 +68,18 @@ return function()
     'vtsls',
     'vue_ls',
   })
+
+  -- 注册LSP附加回调
+  vim.api.nvim_create_autocmd('VimEnter', {
+    callback = function()
+      -- 设置高亮组 @lsp.type.component 链接到 @type 默认组件绿
+      -- vim.api.nvim_command('highlight link @lsp.type.component @type')
+      vim.api.nvim_set_hl(0, '@lsp.type.component', { -- 自定义组件颜色
+        fg = color_table.vue_component_color,
+        bold = true,
+      })
+    end,
+  })
+
   vim.lsp.inlay_hint.enable()
 end
