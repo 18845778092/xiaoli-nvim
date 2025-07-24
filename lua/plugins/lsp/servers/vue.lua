548dbfd3682a790d0bb0b6f851d5b067567d2a84
xiaoli <2585960098@qq.com>
2025-07-24 14:51:01 +0800

    chore: mason

--- lua/plugins/lsp/servers/vue.lua
+++ lua/plugins/lsp/servers/vue.lua
@@ -49,33 +49,7 @@ return function()
     },
   }
 
-  local vue_ls_config = {
-    on_init = function(client)
-      client.handlers['tsserver/request'] = function(_, result, context)
-        local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
-        if #clients == 0 then
-          vim.notify('Could not find `vtsls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
-          return
-        end
-        local ts_client = clients[1]
-
-        local param = unpack(result)
-        local id, command, payload = unpack(param)
-        ts_client:exec_cmd({
-          title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-          command = 'typescript.tsserverRequest',
-          arguments = {
-            command,
-            payload,
-          },
-        }, { bufnr = context.bufnr }, function(_, r)
-          local response_data = { { id, r.body } }
-          ---@diagnostic disable-next-line: param-type-mismatch
-          client:notify('tsserver/response', response_data)
-        end)
-      end
-    end,
-  }
+  local vue_ls_config = {}
 
   vim.lsp.config('vtsls', vtsls_config) -- vue 文件 ts部分支持
   vim.lsp.config('vue_ls', vue_ls_config) -- vue文件 html css支持
