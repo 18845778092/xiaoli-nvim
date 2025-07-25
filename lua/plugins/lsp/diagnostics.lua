c6fe088a153549d36fc71fd383bb379a1debf3ab
xiaoli <2585960098@qq.com>
2025-07-25 09:57:57 +0800

    chore: diagnostic style

--- lua/plugins/lsp/diagnostics.lua
+++ lua/plugins/lsp/diagnostics.lua
@@ -46,18 +46,18 @@ return function()
     },
   })
 
-  vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = color_table.error_color })
-  vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = color_table.warn_color })
-  vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = color_table.info_color })
-  vim.api.nvim_set_hl(0, 'DiagnosticHint', { fg = color_table.hint_color })
-
-  vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextError', { fg = color_table.error_color, bg = 'none' })
-  vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = color_table.warn_color, bg = 'none' })
-  vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextInfo', { fg = color_table.info_color, bg = 'none' })
-  vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextHint', { fg = color_table.hint_color, bg = 'none' })
-
   vim.api.nvim_create_autocmd('VimEnter', {
     callback = function()
+      vim.api.nvim_set_hl(0, 'DiagnosticError', { fg = color_table.error_color })
+      vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = color_table.warn_color })
+      vim.api.nvim_set_hl(0, 'DiagnosticInfo', { fg = color_table.info_color })
+      vim.api.nvim_set_hl(0, 'DiagnosticHint', { fg = color_table.hint_color })
+
+      vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextError', { fg = color_table.error_color, bg = 'none' })
+      vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextWarn', { fg = color_table.warn_color, bg = 'none' })
+      vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextInfo', { fg = color_table.info_color, bg = 'none' })
+      vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextHint', { fg = color_table.hint_color, bg = 'none' })
+
       vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#72c6b1', bg = '#2a2a2a' }) -- 浮动边框
       vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#2a2a2a' }) -- 浮动背景色
     end,
