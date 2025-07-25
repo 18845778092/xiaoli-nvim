f5f95eb84b3de61077e4d5764a585e33cd7c6f44
xiaoli <2585960098@qq.com>
2025-07-25 14:28:34 +0800

    chore: codecompanion config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -6,9 +6,18 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
     'github/copilot.vim',
+    'j-hui/fidget.nvim',
+    'franco-ruggeri/codecompanion-spinner.nvim',
   },
+  init = function()
+    require('helper.codecompanion-fidget-spinner'):init()
+  end,
   config = function()
-    require('codecompanion').setup()
+    require('codecompanion').setup({
+      extensions = {
+        spinner = {},
+      },
+    })
 
     vim.api.nvim_create_autocmd('VimEnter', {
       callback = function()
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,11 +3,10 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
   config = function()
-    require('render-markdown').setup({})
+    require('render-markdown').setup({
+      file_types = { 'markdown', 'codecompanion' },
+    })
     vim.api.nvim_set_hl(0, 'RenderMarkdownH1Bg', { fg = '#4493F8' })
   end,
 }
