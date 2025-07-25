568df94b18787e5814442c2e08106292c83da33e
xiaoli <2585960098@qq.com>
2025-07-25 16:56:17 +0800

    feat: kimi adapter

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -16,6 +16,61 @@ return {
       extensions = {
         spinner = {},
       },
+      strategies = {
+        chat = {
+          adapter = 'kimi',
+        },
+        inline = {
+          adapter = 'kimi',
+        },
+        cmd = {
+          adapter = 'kimi',
+        },
+      },
+      adapters = {
+        kimi = function()
+          return require('codecompanion.adapters').extend('openai', {
+            name = 'kimi',
+            url = 'https://api.moonshot.cn/v1/chat/completions',
+            env = {
+              api_key = vim.fn.getenv('MOONSHOT_API_KEY'),
+            },
+            headers = {
+              ['Content-Type'] = 'application/json',
+              ['Authorization'] = 'Bearer ${api_key}',
+            },
+            parameters = {
+              model = 'kimi-k2-0711-preview',
+              temperature = 0.7,
+              stream = true,
+              max_tokens = 16384,
+            },
+            schema = {
+              model = {
+                order = 1,
+                mapping = 'parameters',
+                type = 'enum',
+                desc = 'Kimi 模型选择',
+                default = 'kimi-k2-0711-preview',
+                choices = {
+                  'kimi-k2-0711-preview',
+                },
+              },
+              temperature = {
+                order = 2,
+                mapping = 'parameters',
+                type = 'number',
+                optional = true,
+                default = 0.7,
+                desc = '控制输出随机性',
+                validate = function(n)
+                  return n >= 0 and n <= 2
+                end,
+              },
+            },
+          })
+        end,
+      },
     })
 
     vim.api.nvim_create_autocmd('VimEnter', {
