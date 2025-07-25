5954c6fdf76dd72bafe770ea45e96c12901ac2f4
xiaoli <2585960098@qq.com>
2025-07-25 19:14:51 +0800

    chore: dropbar hide codecompanion

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -23,5 +23,18 @@ return {
     vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
     vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
     vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    vim.api.nvim_create_autocmd({ 'BufEnter',
+      'BufWinEnter',
+      'FileType',
+      'BufReadPost',
+      'WinEnter' }, {
+      callback = function()
+        local filetype = vim.bo.filetype
+        if filetype == 'codecompanion' then
+          -- 隐藏当前窗口的 winbar
+          vim.wo.winbar = ''
+        end
+      end,
+    })
   end,
 }
