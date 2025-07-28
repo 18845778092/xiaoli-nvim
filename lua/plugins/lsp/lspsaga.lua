a61f2bdb554ce77230b9ba104055fc9b724e7575
xiaoliyo <2585960098@qq.com>
2025-07-28 22:26:14 +0800

    feat: nvim-bqf plugin

--- lua/plugins/lsp/lspsaga.lua
+++ lua/plugins/lsp/lspsaga.lua
@@ -25,7 +25,6 @@ return function()
       vim.keymap.set('n', '<leader>rn', '<cmd>Lspsaga rename<CR>')
       vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<CR>')
       vim.keymap.set('n', '<leader>le', '<cmd>Lspsaga show_buf_diagnostics<CR>')
-      -- vim.keymap.set('n', 'gd', '<cmd>Lspsaga finder def<CR>')
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
       vim.keymap.set('n', 'gr', '<cmd>Lspsaga finder ref<CR>')
     end,
---
+++ lua/plugins/nvim-bqf.lua
@@ -0,0 +1,40 @@
+-- quickfix preview
+
+return {
+  'kevinhwang91/nvim-bqf',
+  ft = 'qf',
+  config = function()
+    require('bqf').setup({
+      auto_enable = true,
+      auto_resize_height = true, -- highly recommended enable
+      preview = {
+        win_height = 24,
+        win_vheight = 12,
+        delay_syntax = 80,
+        border = { '┏', '━', '┓', '┃', '┛', '━', '┗', '┃' },
+        show_title = true,
+        should_preview_cb = function(bufnr, qwinid)
+          return true
+        end,
+      },
+      -- make `drop` and `tab drop` to become preferred
+      func_map = {
+        drop = '<CR>',
+        openc = '',
+        split = '<C-v>',
+        tabdrop = '',
+        -- set to empty string to disable
+        tabc = '',
+        ptogglemode = 'z,',
+        pscrollup = '<S-k>',
+        pscrolldown = '<S-j>',
+      },
+      filter = {
+        fzf = {
+          action_for = { ['ctrl-s'] = 'split', ['ctrl-t'] = 'tab drop' },
+          extra_opts = { '--bind', 'ctrl-o:toggle-all', '--prompt', '> ' },
+        },
+      },
+    })
+  end,
+}
