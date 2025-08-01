0b5ddbb92d8968a33b51fcd0b89bb346c6a4c50a
xiaoliyo <2585960098@qq.com>
2025-08-01 20:55:39 +0800

    style: float window style

--- lua/plugins/global-note.lua
+++ lua/plugins/global-note.lua
@@ -1,6 +1,9 @@
 return {
   'backdround/global-note.nvim',
   config = function()
+    local style = require('core.custom-style')
+    local border = style.border
+
     require('global-note').setup({
       filename = 'global.md',
       directory = '~/notes/',
@@ -20,6 +23,20 @@ return {
           -- All not specified options are used from the root.
         },
       },
+      window_config = function()
+        local window_height = vim.api.nvim_list_uis()[1].height
+        local window_width = vim.api.nvim_list_uis()[1].width
+        return {
+          relative = 'editor',
+          border = border,
+          title = 'Note',
+          title_pos = 'center',
+          width = math.floor(0.7 * window_width),
+          height = math.floor(0.85 * window_height),
+          row = math.floor(0.05 * window_height),
+          col = math.floor(0.15 * window_width),
+        }
+      end,
     })
 
     vim.api.nvim_create_autocmd('VimEnter', {
