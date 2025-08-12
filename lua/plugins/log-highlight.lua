cfd0238ecf4e2431b94774522ebcac38846382d0
xiaoliyo <2585960098@qq.com>
2025-08-12 21:04:04 +0800

    feat: log-highlight plugin

---
+++ lua/plugins/log-highlight.lua
@@ -0,0 +1,26 @@
+return {
+  'fei6409/log-highlight.nvim',
+  config = function()
+    require('log-highlight').setup({
+      ---@type string|string[]
+      -- File extensions
+      extension = 'log',
+
+      ---@type string|string[]
+      -- File names or full file paths
+      filename = {
+        'syslog',
+      },
+
+      ---@type string|string[]
+      -- File path glob patterns
+      -- Note: In Lua, `%` escapes special characters to match them literally.
+      pattern = {
+        '%/var%/log%/.*',
+        'console%-ramoops.*',
+        'log.*%.txt',
+        'logcat.*',
+      },
+    })
+  end,
+}
