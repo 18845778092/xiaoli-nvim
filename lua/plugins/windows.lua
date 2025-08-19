7e1301eda056ed2b7e47834ea6d4f0bccfe12ec3
xiaoliyo <2585960098@qq.com>
2025-08-19 22:03:48 +0800

    feat: windows plugin

---
+++ lua/plugins/windows.lua
@@ -0,0 +1,28 @@
+return {
+  'anuvyklack/windows.nvim',
+  dependencies = {
+    'anuvyklack/middleclass',
+    'anuvyklack/animation.nvim',
+  },
+  config = function()
+    local num = 20
+    vim.o.winwidth = num
+    vim.o.winminwidth = num
+    vim.o.equalalways = false
+    require('windows').setup()
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function()
+        vim.keymap.set('n', '<C-S-m>', function()
+          vim.cmd('WindowsEnableAutowidth')
+          vim.cmd('WindowsMaximize')
+        end, { noremap = true })
+
+        vim.keymap.set('n', '<C-m>', function()
+          vim.cmd('WindowsEnableAutowidth')
+          vim.cmd('WindowsEqualize')
+        end, { noremap = true })
+      end,
+      desc = 'Description of the autocmd',
+    })
+  end,
+}
