5f34ea5201de5823b4d24d1c63d564b12ef94e1a
xiaoliyo <2585960098@qq.com>
2025-08-20 21:16:15 +0800

    chore: base keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,34 +1,30 @@
-vim.g.mapleader = ' '
 local function map(m, k, v)
   vim.keymap.set(m, k, v, { silent = true })
 end
 
 map({ 'v', 'n' }, 'J', '5j')
 map({ 'v', 'n' }, 'K', '5k')
+map({ 'x', 'o', 'n' }, 'H', '^')
+map({ 'x', 'o', 'n' }, 'L', 'g_')
+map('n', '<C-e>', '3<C-e>')
+map('n', '<C-y>', '3<C-y>')
 
--- 快捷操作
-map({ 'v', 'o' }, 'w', 'iw')
-map({ 'v', 'o' }, 'ii', 'i{')
-map({ 'v', 'o' }, 'ai', 'a{')
-map({ 'v', 'o' }, 'b', 'i(')
-map({ 'v', 'o', 'n' }, 'H', '^')
-map({ 'v', 'o', 'n' }, 'L', 'g_')
+map({ 'x', 'o' }, 'w', 'iw')
+map({ 'x', 'o' }, 'ii', 'i{')
+map({ 'x', 'o' }, 'ai', 'a{')
+map({ 'x', 'o' }, 'b', 'i(')
+-- ie 文本对象
+map('v', 'ie', '<Esc>ggVG')
+map('o', 'ie', ':normal! ggVG<CR>')
 
 map('n', '<leader>s', require('helper.smart-select-block'))
-
 map('n', '<leader>a', 'za')
 
-map('v', 'ie', '<Esc>ggVG') -- 全选整个文件内容
-map('n', 'die', 'ggdG') -- 删除整个文件内容
-map('n', 'yie', 'ggyG') -- 复制整个文件内容
-map('n', 'cie', 'ggcG') --
-
--- 取消高亮
-map('n', '<leader>nh', ':nohl<CR>')
-
+map('n', '<leader>nh', ':nohl<CR>') -- 取消高亮
 map('n', '<leader>hh', '<C-w>v') -- 水平新增窗口
 map('n', '<leader>vv', '<C-w>s') -- 垂直新增窗口
---
+map('n', '<leader>ll', '"ayiwoconsole.log(\'<C-R>a:\', <C-R>a);<Esc>') -- Auto log
+-- map('n', '<leader>e', vim.diagnostic.open_float)
 
 -- 禁用默认高亮
 map('n', 'f', '<Nop>')
@@ -36,15 +32,10 @@ map('n', 'F', '<Nop>')
 map('n', 't', '<Nop>')
 map('n', 'T', '<Nop>')
 
--- Auto log
-map('n', '<leader>ll', '"ayiwoconsole.log(\'<C-R>a:\', <C-R>a);<Esc>')
 map('i', 'jk', '<Esc>')
-
 map({ 'n', 'i' }, '<D-s>', function()
   vim.cmd('w')
 end)
-
-map('n', '<space>e', vim.diagnostic.open_float)
 
 vim.keymap.set('n', '=', function()
   vim.cmd('WindowsDisableAutowidth')
